fd96ec0d58c849962a672b651d6332553c7eeaea
Kevin Moses Kenap <kvn.moseskenap@gmail.com>
2024-12-02 21:50:17 +0700

    initial commit project #2

---
+++ app/src/main/java/id/cbm/main/cbm_calculator/ui/mainengineer/MainEngineerActivity.kt
@@ -0,0 +1,12 @@
+package id.cbm.main.cbm_calculator.ui.mainengineer // ktlint-disable package-name
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.cbm.main.cbm_calculator.R
+
+class MainEngineerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_engineer)
+    }
+}
---
+++ app/src/main/java/id/cbm/main/cbm_calculator/ui/mainsales/MainSalesActivity.kt
@@ -0,0 +1,12 @@
+package id.cbm.main.cbm_calculator.ui.mainsales // ktlint-disable package-name
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.cbm.main.cbm_calculator.R
+
+class MainSalesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_sales)
+    }
+}
